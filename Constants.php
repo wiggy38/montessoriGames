872baaa1cf7ca37e1bfa97d8bc5146c45864815b
Wiggy <35220596+wiggy38@users.noreply.github.com>
2023-02-13 12:12:16 +0000

    prod db added

--- Constants.php
+++ Constants.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'm_games_db');
+define('DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USER', 'b33c2f4a3d258e');
+define('DB_PASS', '585da020');
+define('DB_NAME', 'heroku_90715b2116886d5');
 
 ?>
