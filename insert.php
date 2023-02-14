00d5a44f62e07b0938e55987fac2b2bedd961200
Wiggy <35220596+wiggy38@users.noreply.github.com>
2023-02-14 15:26:12 +0000

    test

--- insert.php
+++ insert.php
@@ -1,12 +1,16 @@
 <?php
-
+echo 'a';
 require_once 'DbOperation.php';
+echo 'b';
 $db = new DbOperation();
+echo 'c';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo 'd';
     $res = $db->updateData($_POST);
 
     return true;
   }
 
+  echo 'e';
 ?>
